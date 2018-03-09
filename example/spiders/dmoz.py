1aa683f3fbcfbc37bb7bde73b07b4ddca08380fa
kumanxuan <775121173@qq.com>
2018-03-09 13:20:22 +0800

    restore

--- example/spiders/dmoz.py
+++ example/spiders/dmoz.py
@@ -7,14 +7,12 @@ from example.items import SogouItem
 class DmozSpider(CrawlSpider):
     """Follow categories and extract links."""
     name = 'sogou'
-    allowed_domains = ['zhaopin.com']
-    #start_urls = ['https://pinyin.sogou.com/dict/cate/index/']
-    #尝试去捉取
-    #start_urls = ['https://pinyin.sogou.com/dict/cate/index/']
-    start_urls = ['https://sou.zhaopin.com/jobs/searchresult.ashx?jl=%E5%B9%BF%E4%B8%9C&kw=java&sm=0&p=1',]
+    allowed_domains = ['sogou.com']
+    start_urls = ['https://pinyin.sogou.com/dict/cate/index/',]
+
 
     rules = [
-        Rule(LinkExtractor(restrict_xpaths=('//div/table//tr/td/div/a[1]'),), 'parse_directory',)
+        Rule(LinkExtractor(allow=(r'dict/cate/index')), callback='parse_directory',follow=True),
         #Rule(LinkExtractor(
          #   restrict_css=('.top-cat', '.sub-cat', '.cat-item')
         #), callback='parse_directory', follow=True),
